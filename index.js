7f9c55c6b8e36d7f05ff659b5c11706224494143
ABDUL WASAY <tahawasay1@gmail.com>
2026-03-06 03:58:39 +0500

    databse resolving issue

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
     credentials: true 
 }));
 initSocket(httpServer);
+  connectDB();
 // 3. Routes
 
 app.use("/ECOMERACE/user", userRouter);
@@ -40,7 +41,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
-    connectDB();
     console.log(`Server running on port ${PORT}`);
 });
 module.exports = app;
