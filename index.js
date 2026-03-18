3c945bb108c25f5d3ed7a740db47ffeef3285b3f
ABDUL WASAY <tahawasay1@gmail.com>
2026-03-18 06:22:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,6 @@ const httpServer = createServer(app);
 const RentalRouter = require("./src/services/Route/Rental.route");
 const NotificationRouter = require("./src/services/Route/Notification.route");
 // 1. Middlewares
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "*",
-    credentials: true,
-  }),
-);
-initSocket(httpServer);
-connectDB();
-// 3. Routes
-
-app.use("/ECOMERACE/user", userRouter);
-app.use("/ECOMERACE/cars", carsRouter);
-app.use("/ECOMERACE/rent", RentalRouter);
-app.use("/ECOMERACE/notifications", NotificationRouter);
-
-// 4. Global Error Handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || "Something went wrong";
@@ -39,6 +20,26 @@ app.use((err, req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
   res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
 });
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+initSocket(httpServer);
+connectDB();
+// 3. Routes
+app.use("/ECOMERACE/user", userRouter);
+app.use("/ECOMERACE/cars", carsRouter);
+app.use("/ECOMERACE/rent", RentalRouter);
+app.use("/ECOMERACE/notifications", NotificationRouter);
+
+// 4. Global Error Handler
+
+
 
 const PORT = process.env.PORT || 5000;
 httpServer.listen(PORT, () => {
